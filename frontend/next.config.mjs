de976f27ffd28317c946e58a14d5bd3678732050
KivSystemAdmin <kiv.developers@gmail.com>
2024-09-21 01:36:32 +0700

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // 全ての API routes にマッチ
+    async headers() {
+      return [
+        {
+          headers: [
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "https://aaasobo-managament-system-frontend.vercel.app",
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,POST",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type",
+            },
+          ],
+        },
+      ];
+    },
+  };
+   
+  module.exports = nextConfig;
